b1a77e67bf0bb5e5181ef9018a823438df9da028
MenSeb <sebastien.menard.github@gmail.com>
2022-10-21 19:28:04 -0400

    chore(jest): update jest config to typescript

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
+export default {
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testEnvironment: 'jsdom',
   transform: { '\\.[jt]sx?$': 'ts-jest' },
