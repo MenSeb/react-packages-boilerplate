697b29331ba7bad8dc7c151605b192ff3952b63c
MenSeb <sebastien.menard.github@gmail.com>
2022-10-25 16:20:55 -0400

    chore(jest): remove coverageReporters from jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
-  coverageReporters: ['json'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testEnvironment: 'jsdom',
   transform: { '\\.[jt]sx?$': 'ts-jest' },
