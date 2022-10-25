4beace51fdb061122da261cbd29b9bbfab7aebb1
MenSeb <sebastien.menard.github@gmail.com>
2022-10-25 16:02:02 -0400

    chore(jest): update jest config with coverageReporters lcov

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
+  coverageReporters: ['lcov'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testEnvironment: 'jsdom',
   transform: { '\\.[jt]sx?$': 'ts-jest' },
