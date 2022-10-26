b0a4f92d315f4bfd8b484051a26eee85ab1c3318
MenSeb <sebastien.menard.github@gmail.com>
2022-10-25 23:02:19 -0400

    chore(jest): update config with coverageDirectory

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
+  coverageDirectory: '<rootDir>/coverage',
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testEnvironment: 'jsdom',
   transform: { '\\.[jt]sx?$': 'ts-jest' },
