d3e35241cf8b426f4d0557b82001fc56b18230c9
MenSeb <sebastien.menard.github@gmail.com>
2023-02-23 17:59:12 -0500

    feat(routes): create routes for react router

---
+++ packages/app/src/routes/dashboard.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export function dashboardLoader() {
+  return {};
+}
+
+export function dashboardAction() {
+  return {};
+}
+
+export function Dashboard() {
+  return <section id="dashboard"></section>;
+}
