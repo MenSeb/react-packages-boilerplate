38b843b9018b989060c40c836d8cd2915e352425
MenSeb <sebastien.menard.github@gmail.com>
2023-03-01 10:10:45 -0500

    feat(login): add login route to router

--- packages/app/src/router.tsx
+++ packages/app/src/router.tsx
@@ -4,15 +4,16 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from 'react-router-dom';
-import { About, Error, Home, Layout, Lost } from './pages';
-import { home } from './routes';
+import { About, Error, Home, Layout, Login, Lost } from './pages';
+import { login } from './routes';
 
 export default createBrowserRouter(
   createRoutesFromElements(
     <Route element={<Layout />} path="/">
       <Route errorElement={<Error />}>
-        <Route {...home} element={<Home />} index />
+        <Route element={<Home />} index />
         <Route element={<About />} path="about" />
+        <Route {...login} element={<Login />} path="login" />
         <Route element={<Lost />} path="*" />
       </Route>
     </Route>,
