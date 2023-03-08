48ca6f4ddf54c9e4c0ae195786a77ffbb06eec77
MenSeb <sebastien.menard.github@gmail.com>
2023-03-08 12:25:39 -0500

    test(router): enable navigation testing with MemoryRouter

--- packages/app/__tests__/app.test.tsx
+++ packages/app/__tests__/app.test.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import { screen, render } from '@testing-library/react';
-import { RouterProvider } from 'react-router-dom';
-import router from '../src/router';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter, Routes } from 'react-router-dom';
+import { routes } from '../src/router';
 
 describe('<App />', () => {
-  beforeEach(() => render(<RouterProvider router={router} />));
+  beforeEach(() =>
+    render(
+      <MemoryRouter>
+        <Routes>{routes}</Routes>
+      </MemoryRouter>,
+    ),
+  );
 
   describe('<Layout />', () => {
     it('renders with header', () => {
@@ -24,6 +31,16 @@ describe('<App />', () => {
     it('renders with home page', () => {
       expect(
         screen.getByRole('region', { name: 'home page' }),
+      ).toBeInTheDocument();
+    });
+
+    it('renders with about page', async () => {
+      const user = userEvent.setup();
+
+      await user.click(screen.getByText(/about/i));
+
+      expect(
+        screen.getByRole('region', { name: 'about page' }),
       ).toBeInTheDocument();
     });
   });
--- packages/app/src/router.tsx
+++ packages/app/src/router.tsx
@@ -6,14 +6,14 @@ import {
 } from 'react-router-dom';
 import { About, Error, Home, Layout, Lost } from './pages';
 
-export default createBrowserRouter(
-  createRoutesFromElements(
-    <Route element={<Layout />} path="/">
-      <Route errorElement={<Error />}>
-        <Route element={<Home />} index />
-        <Route element={<About />} path="about" />
-        <Route element={<Lost />} path="*" />
-      </Route>
-    </Route>,
-  ),
+export const routes = (
+  <Route element={<Layout />} path="/">
+    <Route errorElement={<Error />}>
+      <Route element={<Home />} index />
+      <Route element={<About />} path="about" />
+      <Route element={<Lost />} path="*" />
+    </Route>
+  </Route>
 );
+
+export default createBrowserRouter(createRoutesFromElements(routes));
