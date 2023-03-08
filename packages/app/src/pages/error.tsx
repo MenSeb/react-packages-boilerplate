fd77b409d114a63d55f850a850d38d4a3ed64d67
Sébastien Menard <sebastien.menard.github@gmail.com>
2023-03-08 16:30:47 -0500

    feat(router): update app with router using react-router-dom (#40)

---
+++ packages/app/src/pages/error.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { useRouteError, isRouteErrorResponse } from 'react-router-dom';
+
+export default function Error() {
+  const error = useRouteError() as Error;
+
+  return (
+    <section aria-label="error page">
+      <h1>Oops! Seems like there was an unexpected error...</h1>
+      {isRouteErrorResponse(error) ? (
+        <>
+          <p>{error.data}</p>
+          <pre>
+            {error.status} - {error.statusText}
+          </pre>
+        </>
+      ) : (
+        <p>{error.message}</p>
+      )}
+    </section>
+  );
+}
---
+++ packages/app/src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as About } from './about';
+export { default as Error } from './error';
+export { default as Home } from './home';
+export { default as Layout } from './layout';
+export { default as Lost } from './lost';
