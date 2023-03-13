5630360a04d76125a06d727f4e0a4aa48bdd5f96
menseb <sebastien.menard.github@gmail.com>
2023-03-13 15:04:17 -0400

    feat(panel): implement panel component

--- packages/widgets/src/tabs/components/Panel.tsx
+++ packages/widgets/src/tabs/components/Panel.tsx
@@ -1,9 +1,29 @@
 import * as React from 'react';
 
 type PanelProps = {
-  children?: React.ReactNode;
+  children: React.ReactNode;
+  hidden?: boolean;
+  id: string;
+  labelledby: string;
 };
 
-export default function Panel({ children }: PanelProps): JSX.Element {
-  return <div>{children}</div>;
+export default function Panel({
+  children,
+  hidden = true,
+  id,
+  labelledby,
+  ...props
+}: PanelProps): JSX.Element {
+  return (
+    <div
+      {...props}
+      aria-hidden={hidden}
+      aria-labelledby={labelledby}
+      id={id}
+      role="tabpanel"
+      tabIndex={hidden ? -1 : 0}
+    >
+      {children}
+    </div>
+  );
 }
--- packages/widgets/test/tabs/Panel.test.tsx
+++ packages/widgets/test/tabs/Panel.test.tsx
@@ -1,9 +1,67 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
 import { Panel } from '../../src/tabs';
+import { getPanel } from './';
+
+const props = {
+  children: 'children',
+  className: 'classname',
+  id: ' id',
+  labelledby: 'labelledby',
+  style: { color: 'red' },
+};
 
 describe('<Panel />', () => {
-  it('renders correctly', () => {
-    expect(render(<Panel />).container).not.toBeEmptyDOMElement();
+  describe('Default behavior and props', () => {
+    beforeEach(() => render(<Panel {...props} />));
+
+    it('renders with role panel', () => {
+      expect(getPanel()).toBeInTheDocument();
+    });
+
+    it('renders with children', () => {
+      expect(getPanel()).toHaveTextContent(props.children);
+    });
+
+    it('renders with attribute id', () => {
+      expect(getPanel()).toHaveAttribute('id', props.id);
+    });
+
+    it('renders with attribute aria-labelledby', () => {
+      expect(getPanel()).toHaveAttribute('aria-labelledby', props.labelledby);
+    });
+
+    it('renders with attribute aria-hidden to true', () => {
+      expect(getPanel()).toHaveAttribute('aria-hidden', 'true');
+    });
+
+    it('renders with other attributes as className or style', () => {
+      expect(getPanel()).toHaveClass(props.className);
+      expect(getPanel()).toHaveStyle(props.style);
+    });
+  });
+
+  describe('When selected', () => {
+    beforeEach(() => render(<Panel {...props} hidden={false} />));
+
+    it('renders with attribute aria-hidden to false', () => {
+      expect(getPanel()).toHaveAttribute('aria-hidden', 'false');
+    });
+
+    it('renders with attribute tabIndex to 0', () => {
+      expect(getPanel()).toHaveAttribute('tabindex', '0');
+    });
+  });
+
+  describe('When not selected', () => {
+    beforeEach(() => render(<Panel {...props} hidden={true} />));
+
+    it('renders with attribute aria-hidden to true', () => {
+      expect(getPanel()).toHaveAttribute('aria-hidden', 'true');
+    });
+
+    it('renders with attribute tabIndex to -1', () => {
+      expect(getPanel()).toHaveAttribute('tabindex', '-1');
+    });
   });
 });
