067616231d6662ff7002718f1b4cb07327d1ab3e
menseb <sebastien.menard.github@gmail.com>
2023-03-14 09:56:52 -0400

    feat(panellist): implement panellist component

--- packages/widgets/test/tabs/PanelList.test.tsx
+++ packages/widgets/test/tabs/PanelList.test.tsx
@@ -2,8 +2,16 @@ import * as React from 'react';
 import { render } from '@testing-library/react';
 import { PanelList } from '../../src/tabs';
 
+const props = {
+  children: ['children-0', 'children-1'],
+};
+
 describe('<PanelList />', () => {
-  it('renders correctly', () => {
-    expect(render(<PanelList />).container).not.toBeEmptyDOMElement();
+  it('renders with children', () => {
+    const { container } = render(<PanelList {...props} />);
+
+    container.childNodes.forEach((child, index) => {
+      expect(child).toHaveTextContent(props.children[index]);
+    });
   });
 });
