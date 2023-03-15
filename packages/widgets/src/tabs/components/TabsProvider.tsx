78dd216f6b463d59d902fa71e16237993ec61037
MenSeb <sebastien.menard.github@gmail.com>
2023-03-15 15:41:55 -0400

    feat(provider): implement tabs provider

--- packages/widgets/src/tabs/components/TabsProvider.tsx
+++ packages/widgets/src/tabs/components/TabsProvider.tsx
@@ -1,43 +1,26 @@
 import * as React from 'react';
-
-enum TabsActions {
-  next = 'NEXT',
-  prev = 'PREV',
-}
-
-type TabsState = {};
-
-type TabsPayload = {};
-
-type TabsDispatch = {
-  type: TabsActions;
-  payload: object;
-};
-
-const initialTabsState = {};
-
-const TabsContextDispatch = React.createContext<React.Dispatch | null>(null);
-
-const TabsContextState = React.createContext<>(initialTabsState);
-
-function reducer(state: TabsState, payload: TabsPayload): TabsState {
-  return payload ? state : { ...state };
-}
-
-function config(state: TabsState): TabsState {
-  return state;
-}
+import {
+  initializer,
+  reducer,
+  Action,
+  State,
+  TabsContextDispatch,
+  TabsContextState,
+} from '../utilities';
 
 type TabsProviderProps = {
   children: React.ReactNode;
-  initial: TabsState;
+  initial: State;
 };
 
 export default function TabsProvider({
   children,
   initial,
 }: TabsProviderProps): JSX.Element {
-  const [dispatch, state] = React.useReducer(reducer, initial, config);
+  const [state, dispatch] = React.useReducer<
+    React.Reducer<State, Action>,
+    State
+  >(reducer, initial, initializer);
 
   return (
     <TabsContextDispatch.Provider value={dispatch}>
