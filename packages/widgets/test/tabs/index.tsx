3ec0a1b42d1cb0216d2286a398cb55e4b6eed38b
MenSeb <sebastien.menard.github@gmail.com>
2023-03-15 17:52:51 -0400

    test(wrapper): initiate wrapper logic

--- packages/widgets/test/tabs/index.tsx
+++ packages/widgets/test/tabs/index.tsx
@@ -1,4 +1,46 @@
+import * as React from 'react';
 import { screen, within } from '@testing-library/react';
+import { Panel, PanelList, Tab, TabList, Tabs } from '../../src/tabs';
+import {
+  PanelProps,
+  PanelListProps,
+  TabProps,
+  TabListProps,
+  TabsProps,
+} from '../../src/tabs/utilities';
+
+export const numberOfTabs = 5;
+
+export type Wrapper = {
+  propsPanel: PanelProps;
+  propsPanelList: PanelListProps;
+  propsTab: TabProps;
+  propsTabList: TabListProps;
+  propsTabs: TabsProps;
+};
+
+export function wrapper({
+  propsPanel = {},
+  propsPanelList = {},
+  propsTab = {},
+  propsTabList = {},
+  propsTabs = {},
+}: Wrapper) {
+  return (
+    <Tabs {...propsTabs}>
+      <TabList {...propsTabList}>
+        {Array.from({ length: numberOfTabs }, (_, index) => (
+          <Tab key={index} {...propsTab} />
+        ))}
+      </TabList>
+      <PanelList {...propsPanelList}>
+        {Array.from({ length: numberOfTabs }, (_, index) => (
+          <Panel key={index} {...propsPanel} />
+        ))}
+      </PanelList>
+    </Tabs>
+  );
+}
 
 export function getAllPanels(hidden = true): HTMLElement[] {
   return screen.getAllByRole('tabpanel', { hidden });
