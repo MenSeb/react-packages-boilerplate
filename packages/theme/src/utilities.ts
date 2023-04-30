4eec06400aa5f5cfb9b3b5f43dfdbc36c51197d3
MenSeb <sebastien.menard.github@gmail.com>
2023-04-29 20:16:45 -0400

    feat(initializer): create initializer function

--- packages/theme/src/utilities.ts
+++ packages/theme/src/utilities.ts
@@ -1,4 +1,16 @@
+import { ThemeState } from './types';
+
 export const THEME_CONTEXT_ERROR =
   'useThemeContext should be used within ThemeProvider.';
 export const THEME_QUERY_DARK = '(prefers-color-scheme: dark)';
 export const THEME_STORAGE_KEY = 'color-scheme';
+
+export function initializer(): ThemeState {
+  const theme = localStorage.getItem(THEME_STORAGE_KEY);
+
+  if (theme === 'light' || theme === 'dark') return { theme };
+
+  return {
+    theme: window.matchMedia(THEME_QUERY_DARK).matches ? 'dark' : 'light',
+  };
+}
