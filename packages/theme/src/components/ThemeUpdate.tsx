795d4e1c48db1fbb5284d710250e72ce1a8e5a6d
MenSeb <sebastien.menard.github@gmail.com>
2023-04-29 20:17:57 -0400

    feat(component): create ThemeUpdate component

---
+++ packages/theme/src/components/ThemeUpdate.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import * as ContextReducer from '@react/context-reducer';
+import { THEME_QUERY_DARK, THEME_STORAGE_KEY } from '../utilities';
+
+export default function ThemeUpdate(): null {
+  const { dispatch, state } = ContextReducer.useContextReducer();
+
+  React.useEffect(() => {
+    localStorage.setItem(THEME_STORAGE_KEY, state.theme);
+  }, [state.theme]);
+
+  React.useEffect(() => {
+    function updateTheme() {
+      dispatch.toggleTheme();
+    }
+
+    const mediaQuery = window.matchMedia(THEME_QUERY_DARK);
+
+    mediaQuery.addEventListener('change', updateTheme);
+
+    return () => {
+      mediaQuery.removeEventListener('change', updateTheme);
+    };
+  }, [dispatch]);
+
+  return null;
+}
