b6fa15a74205542041925e7dacc3e60964a0f63b
MenSeb <sebastien.menard.github@gmail.com>
2023-05-02 11:11:44 -0400

    test(buttons): test theme buttons

---
+++ packages/theme/__tests__/components/ButtonLight.test.tsx
@@ -0,0 +1,39 @@
+import userEvent from '@testing-library/user-event';
+import { act } from '@testing-library/react';
+import { ButtonLight } from '../../src/components';
+import { THEME_STORAGE_KEY } from '../../src/utilities';
+import { createRender, getButton } from '..';
+
+const renderButtonLight = createRender(ButtonLight);
+
+describe('<ThemeButtonLight />', () => {
+  it('renders with attribute data-theme to light', () => {
+    renderButtonLight();
+
+    expect(getButton()).toHaveAttribute('data-theme', 'light');
+  });
+
+  it('renders with aria-pressed to true when theme is light', () => {
+    renderButtonLight();
+
+    expect(getButton()).toHaveAttribute('aria-pressed', 'true');
+  });
+
+  it('renders with aria-pressed to false when theme is dark', () => {
+    localStorage.setItem(THEME_STORAGE_KEY, 'dark');
+
+    renderButtonLight();
+
+    expect(getButton()).toHaveAttribute('aria-pressed', 'false');
+  });
+
+  it('renders with click handler to set the theme to light', async () => {
+    localStorage.setItem(THEME_STORAGE_KEY, 'dark');
+
+    renderButtonLight();
+
+    await act(async () => await userEvent.click(getButton()));
+
+    expect(getButton()).toHaveAttribute('aria-pressed', 'true');
+  });
+});
---
+++ packages/theme/__tests__/components/ButtonToggle.test.tsx
@@ -0,0 +1,43 @@
+import userEvent from '@testing-library/user-event';
+import { act } from '@testing-library/react';
+import { ButtonToggle } from '../../src/components';
+import { THEME_STORAGE_KEY } from '../../src/utilities';
+import { createRender, getButton } from '..';
+
+const renderButtonToggle = createRender(ButtonToggle);
+
+describe('<ThemeButtonToggle />', () => {
+  it('renders with attribute data-theme set to theme', () => {
+    renderButtonToggle();
+
+    expect(getButton()).toHaveAttribute('data-theme', 'light');
+  });
+
+  it('renders with aria-pressed to false when the theme is light', () => {
+    renderButtonToggle();
+
+    expect(getButton()).toHaveAttribute('aria-pressed', 'false');
+  });
+
+  it('renders with aria-pressed to true when the theme is dark', () => {
+    localStorage.setItem(THEME_STORAGE_KEY, 'dark');
+
+    renderButtonToggle();
+
+    expect(getButton()).toHaveAttribute('aria-pressed', 'true');
+  });
+
+  it('renders with click handler to toggle the theme', async () => {
+    renderButtonToggle();
+
+    expect(getButton()).toHaveAttribute('data-theme', 'light');
+
+    await act(async () => await userEvent.click(getButton()));
+
+    expect(getButton()).toHaveAttribute('data-theme', 'dark');
+
+    await act(async () => await userEvent.click(getButton()));
+
+    expect(getButton()).toHaveAttribute('data-theme', 'light');
+  });
+});
