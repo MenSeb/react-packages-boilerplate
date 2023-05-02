26dd2ef49f6983bd27699a84cb9a42b000baac01
MenSeb <sebastien.menard.github@gmail.com>
2023-05-01 23:31:12 -0400

    feat(buttons): create theme buttons

---
+++ packages/theme/src/components/ThemeButtonDark.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import ContextReducer from '../setup';
+
+export default function ThemeButtonDark({
+  children,
+  ...props
+}: React.HTMLProps<HTMLButtonElement>): JSX.Element {
+  const { state, dispatch } = ContextReducer.useContextReducer();
+
+  return (
+    <button
+      {...props}
+      aria-pressed={state.theme === 'dark'}
+      data-theme="dark"
+      onClick={() => dispatch.setThemeDark()}
+      type="button"
+    >
+      {children}
+    </button>
+  );
+}
---
+++ packages/theme/src/components/ThemeButtonToggle.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import ContextReducer from '../setup';
+
+export default function ThemeButtonToggle({
+  children,
+  ...props
+}: React.HTMLProps<HTMLButtonElement>): JSX.Element {
+  const { state, dispatch } = ContextReducer.useContextReducer();
+
+  return (
+    <button
+      {...props}
+      aria-pressed={state.theme === 'dark'}
+      data-theme={state.theme}
+      onClick={() => dispatch.toggleTheme()}
+      type="button"
+    >
+      {children}
+    </button>
+  );
+}
