c50702027663debbb3b7e95ce489639d249aa1e3
MenSeb <sebastien.menard.github@gmail.com>
2023-05-01 23:52:39 -0400

    feat(exports): exports theme components

---
+++ packages/theme/src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as ButtonDark } from './ThemeButtonDark';
+export { default as ButtonLight } from './ThemeButtonLight';
+export { default as ButtonToggle } from './ThemeButtonToggle';
+export { default as IconDark } from './ThemeIconDark';
+export { default as IconLight } from './ThemeIconLight';
+export { default as Provider } from './ThemeProvider';
