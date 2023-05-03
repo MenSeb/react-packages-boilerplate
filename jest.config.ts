9d47b35c4baf3d862e29c5df3245c750bd1b09c5
Sébastien Menard <sebastien.menard.github@gmail.com>
2023-05-03 00:11:25 -0400

    chore(jest): update jest config with Config and collectCoverageFrom (#68)

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,18 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-export default {
+import type { Config } from 'jest';
+
+const config: Config = {
+  collectCoverageFrom: [
+    '<rootDir>/packages/**/src/*.{ts,tsx}',
+    '<rootDir>/packages/**/src/**/*.{ts,tsx}',
+    '!<rootDir>/packages/**/src/index.{ts,tsx}',
+    '!<rootDir>/packages/**/src/**/index.{ts,tsx}',
+  ],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['^(.(?!\\.test\\.))*$'],
   transform: { '\\.[jt]sx?$': 'ts-jest' },
   verbose: true,
 };
+
+export default config;
