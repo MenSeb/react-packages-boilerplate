64bbd426e892d44113a0c918e7d8ee0251d8ddc4
MenSeb <sebastien.menard.github@gmail.com>
2023-05-03 23:08:45 -0400

    test(tab): test tab component

---
+++ packages/widgets/__tests__/tabs/components/Tab.test.tsx
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import { Tab } from '../../../src/tabs';
+import { getTab } from '../';
+
+const props = {
+  children: 'children',
+  className: 'classname',
+  controls: 'controls',
+  id: ' id',
+  removable: false,
+  selected: false,
+  style: { color: 'red' },
+};
+
+describe('<Tab />', () => {
+  describe('Default behavior and props', () => {
+    beforeEach(() => render(<Tab {...props} />));
+
+    it('renders with role tab', () => {
+      expect(getTab()).toBeInTheDocument();
+    });
+
+    it('renders with children', () => {
+      expect(getTab()).toHaveTextContent(props.children);
+    });
+
+    it('renders with attribute id', () => {
+      expect(getTab()).toHaveAttribute('id', props.id);
+    });
+
+    it('renders with attribute aria-controls', () => {
+      expect(getTab()).toHaveAttribute('aria-controls', props.controls);
+    });
+
+    it('renders with other attributes', () => {
+      expect(getTab()).toHaveClass(props.className);
+      expect(getTab()).toHaveStyle(props.style);
+    });
+  });
+
+  describe('When selected', () => {
+    beforeEach(() => render(<Tab {...props} selected={true} />));
+
+    it('renders with attribute aria-selected to true', () => {
+      expect(getTab()).toHaveAttribute('aria-selected', 'true');
+    });
+
+    it('renders with attribute tabIndex to 0', () => {
+      expect(getTab()).toHaveAttribute('tabindex', '0');
+    });
+  });
+
+  describe('When not selected', () => {
+    beforeEach(() => render(<Tab {...props} selected={false} />));
+
+    it('renders with attribute aria-selected to false', () => {
+      expect(getTab()).toHaveAttribute('aria-selected', 'false');
+    });
+
+    it('renders with attribute tabIndex to -1', () => {
+      expect(getTab()).toHaveAttribute('tabindex', '-1');
+    });
+  });
+
+  describe('When removable', () => {
+    beforeEach(() => render(<Tab {...props} removable={true} />));
+
+    it('renders with attribute data-removable to true', () => {
+      expect(getTab()).toHaveAttribute('data-removable', 'true');
+    });
+  });
+
+  describe('When not removable', () => {
+    beforeEach(() => render(<Tab {...props} removable={false} />));
+
+    it('renders with attribute data-removable to false', () => {
+      expect(getTab()).toHaveAttribute('data-removable', 'false');
+    });
+  });
+});
