81b5afc2edafe333bed61c40d11dcf15381f689d
MenSeb <sebastien.menard.github@gmail.com>
2023-05-04 19:18:54 -0400

    test(provider): test click tab with provider

---
+++ packages/widgets/__tests__/tabs/components/Provider.test.tsx
@@ -0,0 +1,23 @@
+// import userEvent from '@testing-library/user-event';
+// import { act } from '@testing-library/react';
+import { createRenderTabs, getTab, getPanel, Widget } from '../';
+
+const renderWidget = createRenderTabs(Widget);
+
+// const user = userEvent.setup();
+
+describe('<TabsProvider>', () => {
+  beforeEach(() => renderWidget());
+
+  describe('When user click a tab', () => {
+    it('focuses the tab and activates its associated tab panel', async () => {
+      expect(getTab()).not.toHaveFocus();
+      expect(getPanel()).toHaveAttribute('aria-hidden', 'true');
+
+      // await act(async () => await user.click(getTab()));
+
+      // expect(getTab()).toHaveFocus();
+      // expect(getPanel()).toBeVisible();
+    });
+  });
+});
