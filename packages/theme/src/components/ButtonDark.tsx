865177782ed7daf4ba5e5184e411dd5344a47317
Sébastien Menard <sebastien.menard.github@gmail.com>
2023-05-09 12:31:27 -0400

    refactor(theme): update theme logic with enum Theme (#78)

--- packages/theme/src/components/ButtonDark.tsx
+++ packages/theme/src/components/ButtonDark.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react';
 import ContextReducer from '../setup';
+import { Theme } from '../types';
 
 export default function ButtonDark({
   children,
   ...props
-}: React.HTMLProps<HTMLButtonElement>): JSX.Element {
+}: React.ComponentProps<'button'>) {
   const { state, dispatch } = ContextReducer.useContextReducer();
 
   return (
     <button
       {...props}
-      aria-pressed={state.theme === 'dark'}
-      data-theme="dark"
+      aria-pressed={state.theme === Theme.DARK}
+      data-theme={Theme.DARK}
       onClick={dispatch.setThemeDark}
       type="button"
     >
--- packages/theme/src/components/ButtonLight.tsx
+++ packages/theme/src/components/ButtonLight.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react';
 import ContextReducer from '../setup';
+import { Theme } from '../types';
 
 export default function ButtonLight({
   children,
   ...props
-}: React.HTMLProps<HTMLButtonElement>): JSX.Element {
+}: React.ComponentProps<'button'>) {
   const { state, dispatch } = ContextReducer.useContextReducer();
 
   return (
     <button
       {...props}
-      aria-pressed={state.theme === 'light'}
-      data-theme="light"
+      aria-pressed={state.theme === Theme.LIGHT}
+      data-theme={Theme.LIGHT}
       onClick={dispatch.setThemeLight}
       type="button"
     >
