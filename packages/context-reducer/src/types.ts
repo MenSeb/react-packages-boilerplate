e08da0da7aa6fd5fb370df6e5262fdf74793430e
MenSeb <sebastien.menard.github@gmail.com>
2023-05-10 14:53:01 -0400

    refactor(types): reorder and update types

--- packages/context-reducer/src/types.ts
+++ packages/context-reducer/src/types.ts
@@ -1,3 +1,27 @@
+export type GenericAction<Type, Payload> = {
+  type: Type;
+  payload: Payload;
+};
+
+export type GenericActions<Actions, State, Payload> = {
+  [key in keyof Actions]:
+    | ((state: State) => State)
+    | ((state: State, payload: Payload) => State)
+    | ((state: State, payload?: Payload) => State);
+};
+
+export type GenericDispatcher<Actions, Payload> = {
+  [key in keyof Actions]: (payload?: Payload) => void;
+};
+
+export type Initializer<State> = (state?: State | Partial<State>) => State;
+
+export type Options<Actions, State, Payload> = {
+  actions: GenericActions<Actions, State, Payload>;
+  defaultState?: Partial<State>;
+  initializer: Initializer<State>;
+};
+
 export type ConsumerStateProps<State> = {
   children: ({ state }: { state: State }) => React.ReactNode;
 };
@@ -22,28 +46,4 @@ export type ConsumerReducerProps<Actions, State, Payload> = {
 
 export type ProviderProps<S> = React.PropsWithChildren & {
   initialState?: Partial<S>;
-};
-
-export type Initializer<State> = (state?: State | Partial<State>) => State;
-
-export type Options<Actions, State, Payload> = {
-  actions: GenericActions<Actions, State, Payload>;
-  defaultState?: Partial<State>;
-  initializer: Initializer<State>;
-};
-
-export type GenericActions<Actions, State, Payload> = {
-  [key in keyof Actions]:
-    | ((state: State) => State)
-    | ((state: State, payload: Payload) => State)
-    | ((state: State, payload?: Payload) => State);
-};
-
-export type GenericAction<Type, Payload> = {
-  type: Type;
-  payload: Payload;
-};
-
-export type GenericDispatcher<Actions, Payload> = {
-  [key in keyof Actions]: (payload?: Payload) => void;
 };
--- packages/context-reducer/src/utilities.ts
+++ packages/context-reducer/src/utilities.ts
@@ -1,14 +1,14 @@
-import { GenericAction, GenericActions, GenericDispatcher } from './types';
+import * as Types from './types';
 
 export function createError(source: string): Error {
   return new Error(`Context missing, ${source} must be use with provider.`);
 }
 
 export function createDispatcher<Actions, State, Payload>(
-  actions: GenericActions<Actions, State, Payload>,
-  dispatch: React.Dispatch<GenericAction<keyof Actions, Payload>>,
+  actions: Types.GenericActions<Actions, State, Payload>,
+  dispatch: React.Dispatch<Types.GenericAction<keyof Actions, Payload>>,
 ) {
-  const dispatcher = {} as GenericDispatcher<Actions, Payload>;
+  const dispatcher = {} as Types.GenericDispatcher<Actions, Payload>;
 
   for (const type of Object.keys(actions) as (keyof Actions)[])
     dispatcher[type] = (payload = {} as Payload) => dispatch({ payload, type });
@@ -17,11 +17,11 @@ export function createDispatcher<Actions, State, Payload>(
 }
 
 export function createReducer<Actions, State, Payload>(
-  actions: GenericActions<Actions, State, Payload>,
+  actions: Types.GenericActions<Actions, State, Payload>,
 ) {
   return function reducer(
     state: State,
-    action: GenericAction<keyof Actions, Payload>,
+    action: Types.GenericAction<keyof Actions, Payload>,
   ) {
     return actions[action.type](state, action.payload);
   };
