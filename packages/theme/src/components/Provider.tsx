e1e70e3babd47376e49b9a2b714c3f978abd7e8c
MenSeb <sebastien.menard.github@gmail.com>
2023-05-19 11:04:05 -0400

    refactor(theme): update theme package with useMatchMedia

--- packages/theme/src/components/Provider.tsx
+++ packages/theme/src/components/Provider.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
+import * as ReactHooks from '@react/hooks';
 import ContextReducer from '../setup';
+import { Theme } from '../types';
+import { THEME_QUERY_DARK } from '../utilities';
 import Update from './Update';
 
 export default function Provider({
   children,
 }: React.PropsWithChildren): JSX.Element {
+  const { matches } = ReactHooks.useMatchMedia(THEME_QUERY_DARK);
+
   return (
-    <ContextReducer.Provider>
+    <ContextReducer.Provider
+      initialState={{ theme: matches ? Theme.DARK : undefined }}
+    >
       <Update />
       {children}
     </ContextReducer.Provider>
