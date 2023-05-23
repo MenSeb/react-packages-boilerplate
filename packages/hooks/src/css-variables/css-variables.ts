12b0705023353da7755cefb96285a5ca8b1e61dd
MenSeb <sebastien.menard.github@gmail.com>
2023-05-23 18:20:37 -0400

    refactor(css-variables): update hook with refObject

--- packages/hooks/src/css-variables/css-variables.ts
+++ packages/hooks/src/css-variables/css-variables.ts
@@ -1,11 +1,22 @@
 import * as React from 'react';
 
-export default function useVariablesCSS(
+function setVariablesCSS(
   variables: Record<string, string>,
   element: HTMLElement,
 ) {
+  for (const variable in variables)
+    element.style.setProperty(`--${variable}`, variables[variable]);
+}
+
+export default function useVariablesCSS(
+  variables: Record<string, string>,
+  elementOrRef: HTMLElement | React.RefObject<HTMLElement>,
+) {
   React.useEffect(() => {
-    for (const variable in variables)
-      element.style.setProperty(`--${variable}`, variables[variable]);
-  }, [element, variables]);
+    if (elementOrRef instanceof HTMLElement)
+      return setVariablesCSS(variables, elementOrRef);
+
+    if (elementOrRef.current !== null)
+      return setVariablesCSS(variables, elementOrRef.current);
+  }, [elementOrRef, variables]);
 }
