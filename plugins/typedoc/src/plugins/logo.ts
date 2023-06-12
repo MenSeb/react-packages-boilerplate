1c45508be6c95a017e353ad87b942d4324a0a21f
MenSeb <sebastien.menard.github@gmail.com>
2023-06-12 16:37:32 -0400

    feat(typedoc): update plugin with jsdom

--- plugins/typedoc/src/plugins/logo.ts
+++ plugins/typedoc/src/plugins/logo.ts
@@ -1,5 +1,6 @@
 import { Application, PageEvent, ParameterType, Renderer } from 'typedoc';
 import { copyFileSync, readdirSync } from 'node:fs';
+import { JSDOM } from 'jsdom';
 
 export default function logo(app: Application) {
   app.options.addDeclaration({
@@ -9,8 +10,20 @@ export default function logo(app: Application) {
     defaultValue: 'logo.svg',
   });
 
-  app.renderer.on(PageEvent.END, (/*page: PageEvent*/) => {
-    // update img src with {packageName}.svg
+  app.renderer.on(PageEvent.END, (page: PageEvent) => {
+    const dom = new JSDOM(page.contents);
+    const {
+      window: { document },
+    } = dom;
+    const title = document.querySelector('h1') as HTMLHeadingElement;
+    const text = title.textContent as string;
+    const [, packageName] = text.split('/');
+
+    document
+      .querySelector('img[src="logo.svg"]')
+      ?.setAttribute('src', `${packageName}.svg`);
+
+    page.contents = dom.serialize();
   });
 
   app.renderer.on(Renderer.EVENT_END, () => {
