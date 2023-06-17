85efafc42ed56639b4e97bc8128e1d41f5d2580c
Sébastien Menard <sebastien.menard.github@gmail.com>
2023-06-17 17:49:29 -0400

    revert(plugins): revert typedoc plugins changes and documentation (#122)

--- plugins/typedoc/src/index.ts
+++ plugins/typedoc/src/index.ts
@@ -1,2 +1,22 @@
+import { Application } from 'typedoc';
+import * as plugins from './plugins';
+
 export * from './plugins';
-export * from './utilities';
+
+/**
+ * The type for plugin.
+ */
+export type Plugin = (app: Application) => void;
+
+/**
+ * Loads Typedoc application with plugins.
+ *
+ * You shouldn't be modifying this function to use a new plugin.
+ *
+ * Instead, export your plugin via the plugins index file.
+ *
+ * @param app the Typedoc application.
+ */
+export function load(app: Application): void {
+  for (const plugin of Object.values<Plugin>(plugins)) plugin(app);
+}
--- plugins/typedoc/src/plugins/toc.ts
+++ plugins/typedoc/src/plugins/toc.ts
@@ -1,18 +1,15 @@
 import { Application, PageEvent } from 'typedoc';
 
-export const REGEXP_LINK = /<li><a href="#(?!md:).*">.+<\/a>/g;
-
 /**
  * Prefixes each table of contents link of the readme file.
  *
  * @param app the Typedoc application.
- * @category plugins
  */
 export default function pluginTableOfContents(app: Application): void {
   app.renderer.on(PageEvent.END, (page: PageEvent) => {
     if (page.contents === undefined) return;
 
-    const links = page.contents.match(REGEXP_LINK);
+    const links = page.contents.match(/<li><a href="#(?!md:).*">.+<\/a>/g);
 
     if (links === null) return;
 
