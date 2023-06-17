a900945cf8c6ac90a8181d385f1ab8ecb13b7460
MenSeb <sebastien.menard.github@gmail.com>
2023-06-17 17:45:53 -0400

    revert(plugins): revert typedoc plugins changes and documentation

--- plugins/typedoc/src/plugins/logo.ts
+++ plugins/typedoc/src/plugins/logo.ts
@@ -1,11 +1,5 @@
 import { Application, PageEvent, Renderer, RendererEvent } from 'typedoc';
 import { copyFileSync, existsSync, mkdirSync } from 'node:fs';
-import {
-  createURL,
-  extractFileName,
-  extractFolderName,
-  formatFileExtension,
-} from '../utilities';
 
 const FILE_LOGO = 'logo.svg';
 const SEGMENT_ASSETS = 'assets';
@@ -18,23 +12,68 @@ const PATH_MODULES = createURL(SEGMENT_MODULES, '_');
 const URL_LOGO = createURL(PATH_ICONS, FILE_LOGO);
 
 /**
+ * Creates a URL with a slash between each segment.
+ *
+ * @param segments the url segments.
+ * @returns the url with slashes.
+ */
+function createURL(...segments: string[]): string {
+  let url = '';
+
+  for (const segment of segments) url += `${segment}/`;
+
+  return url.slice(0, -1);
+}
+
+/**
+ * Extracts the module name from its url.
+ *
+ * E.G. modules/_workspace_my_module.html => my_module
+ *
+ * @param url the module url.
+ * @returns the module name.
+ */
+function extractModule(url: string): string {
+  return url.substring(
+    url.indexOf('_', PATH_MODULES.length) + 1,
+    url.lastIndexOf('.'),
+  );
+}
+
+/**
+ * Extracts the workspace name from its url.
+ *
+ * E.G. modules/_workspace_my_module.html => workspace
+ *
+ * @param url the module url.
+ * @returns the workspace name.
+ */
+function extractWorkspace(url: string): string {
+  return url.substring(
+    PATH_MODULES.length,
+    url.indexOf('_', PATH_MODULES.length),
+  );
+}
+
+/**
  * Creates the logo url of a module.
  *
- * @param url the page url.
+ * @param url the module url.
  * @returns the logo url.
  */
 function createLogoURL(url: string): string {
-  const file = extractFileName(url, PATH_MODULES);
-  const folder = extractFolderName(url, PATH_MODULES);
-
-  return createURL('..', PATH_ICONS, folder, formatFileExtension(file, 'svg'));
+  return createURL(
+    '..',
+    PATH_ICONS,
+    extractWorkspace(url),
+    `${extractModule(url)}.svg`,
+  );
 }
 
 /**
  * Copies and links modules logo.
  *
  * @param app the Typedoc application.
- * @category plugins
  */
 export default function pluginLogo(app: Application): void {
   app.renderer.on(PageEvent.END, (page: PageEvent) => {
@@ -60,15 +99,15 @@ export default function pluginLogo(app: Application): void {
     for (const { url } of event.urls) {
       if (!url.includes(PATH_MODULES)) continue;
 
-      const file = extractFileName(url, PATH_MODULES);
-      const folder = extractFolderName(url, PATH_MODULES);
-      const path = createURL(PATH_LOGOS, folder);
+      const module = extractModule(url);
+      const workspace = extractWorkspace(url);
+      const path = createURL(PATH_LOGOS, workspace);
 
       if (!existsSync(path)) mkdirSync(path);
 
       copyFileSync(
-        createURL(folder, file.replace(/_/g, '-'), FILE_LOGO),
-        createURL(path, formatFileExtension(file, 'svg')),
+        createURL(workspace, module.replace(/_/g, '-'), FILE_LOGO),
+        createURL(path, `${module}.svg`),
       );
     }
   });
