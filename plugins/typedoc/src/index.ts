aaa9b7c1228d3e8872bddb7f5c0a1fcf765d2137
MenSeb <sebastien.menard.github@gmail.com>
2023-06-17 10:36:45 -0400

    chore(load): improve loading of plugins

--- plugins/typedoc/src/index.ts
+++ plugins/typedoc/src/index.ts
@@ -1,15 +1,4 @@
-import { Application } from 'typedoc';
-import * as plugins from './plugins';
+export * from './plugins';
+export * from './utilities';
 
-/**
- * Loads each Typedoc plugins.
- *
- * You shouldn't be modifying this function to use a new plugin.
- *
- * Instead, export your plugin via the plugins index file.
- *
- * @param app the Typedoc application.
- */
-export function load(app: Application): void {
-  for (const plugin of Object.values(plugins)) plugin(app);
-}
+export * from './load';
