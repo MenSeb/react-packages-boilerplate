f4966c213c26d2fa1ae27abfc9597ebe938cb2fd
MenSeb <sebastien.menard.github@gmail.com>
2023-07-21 15:41:40 -0400

    chore(styles): import styles

--- apps/webfolio/src/index.tsx
+++ apps/webfolio/src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+import './styles';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
