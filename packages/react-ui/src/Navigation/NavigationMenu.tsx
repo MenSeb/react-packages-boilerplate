bcf63c5610d04ae126c38ac21fb1e0704b04f97d
MenSeb <sebastien.menard.github@gmail.com>
2023-07-25 16:27:29 -0400

    chore(ui): update & test navigation components

--- packages/react-ui/src/Navigation/NavigationMenu.tsx
+++ packages/react-ui/src/Navigation/NavigationMenu.tsx
@@ -1,26 +1,30 @@
 import * as React from 'react';
 import { NavLink, NavLinkProps } from 'react-router-dom';
 import { Navigation, NavigationProps } from '.';
-import { ListUnordered } from '..';
+import { classNames, ClassNames, ListUnordered } from '..';
 
 /**
  * The props types for component {@link NavigationMenu}
  */
 export type NavigationMenuProps = NavigationProps & {
   links: NavLinkProps[];
-};
+} & ClassNames;
 
 /**
  * Renders a navigation menu.
  *
  * @category Component
  */
-export function NavigationMenu({ links, ...props }: NavigationMenuProps) {
+export function NavigationMenu({
+  className,
+  links,
+  ...props
+}: NavigationMenuProps) {
   return (
-    <Navigation {...props}>
-      <ListUnordered>
+    <Navigation {...props} className={classNames('navigation-menu', className)}>
+      <ListUnordered className="navigation-menu-list">
         {links.map((link, index) => (
-          <NavLink {...link} key={index} />
+          <NavLink {...link} className="navigation-menu-link" key={index} />
         ))}
       </ListUnordered>
     </Navigation>
