48cc7d0261d916c7f66ed8e052f50d5a5c3a2038
MenSeb <sebastien.menard.github@gmail.com>
2023-07-27 13:08:34 -0400

    chore(webfolio): implement about page and section

--- apps/webfolio/src/components/index.ts
+++ apps/webfolio/src/components/index.ts
@@ -1,2 +1,3 @@
+export * from './About';
 export * from './Navigation';
 export * from './Services';
--- apps/webfolio/src/pages/about.tsx
+++ apps/webfolio/src/pages/about.tsx
@@ -1,5 +1,42 @@
 import * as React from 'react';
+import {
+  CallToAction,
+  Container,
+  Heading,
+  Page,
+  Region,
+  Separator,
+  Text,
+} from '@packages/react-ui';
+import { About } from '../components';
 
-export default function About() {
-  return <section aria-label="about page"></section>;
+export default function AboutPage() {
+  return (
+    <Page label="about page" className="page-about">
+      <header>
+        <Container>
+          <Heading>about</Heading>
+          <Text>Something about me.</Text>
+          <CallToAction emphasize to="contact">
+            get in touch
+          </CallToAction>
+        </Container>
+      </header>
+
+      <Separator />
+
+      <Region className="region-about" label="about">
+        <About
+          author="Sébastien Menard"
+          image="assets/svgo/undraw_programming.svg"
+          title="Something about me"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et malesuada libero. Duis in metus erat. Vivamus accumsan mi ac nibh dapibus placerat. Maecenas facilisis ex eu magna finibus, at porta nulla vehicula."
+        >
+          <CallToAction className="about-cta" emphasize to="about">
+            about me
+          </CallToAction>
+        </About>
+      </Region>
+    </Page>
+  );
 }
