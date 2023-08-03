626fa4acca4e8db8adf4a17e5918cede39115ccf
MenSeb <sebastien.menard.github@gmail.com>
2023-08-03 11:04:03 -0400

    chore(images): expose svg images

--- apps/webfolio/src/utilities/images.ts
+++ apps/webfolio/src/utilities/images.ts
@@ -1,3 +1,13 @@
-import ImageProgressiveApp from '../../public/assets/images/progressive-app.svg';
+import ImageHomeBlog from '../../public/assets/images/home-blog.svg';
+import ImageHomeHeader from '../../public/assets/images/home-header.svg';
+import ImageHomeProgramming from '../../public/assets/images/home-programming.svg';
+import ImageHomeProjects from '../../public/assets/images/home-projects.svg';
+import ImageHomeServices from '../../public/assets/images/home-services.svg';
 
-export { ImageProgressiveApp };
+export {
+  ImageHomeBlog,
+  ImageHomeHeader,
+  ImageHomeProgramming,
+  ImageHomeProjects,
+  ImageHomeServices,
+};
