850f758420ad22b461ee909a2be8229cb41cab89
MenSeb <sebastien.menard.github@gmail.com>
2023-08-03 13:31:50 -0400

    chore(images): expose all images SVG

--- apps/webfolio/src/utilities/images.ts
+++ apps/webfolio/src/utilities/images.ts
@@ -3,11 +3,23 @@ import ImageHomeHeader from '../../public/assets/images/home-header.svg';
 import ImageHomeProgramming from '../../public/assets/images/home-programming.svg';
 import ImageHomeProjects from '../../public/assets/images/home-projects.svg';
 import ImageHomeServices from '../../public/assets/images/home-services.svg';
+import ImageContactHeader from '../../public/assets/svgo/undraw_contact_us.svg';
+import ImageAboutHeader from '../../public/assets/svgo/undraw_hello.svg';
+import ImageProjectsHeader from '../../public/assets/svgo/undraw_online_stats.svg';
+import ImageAboutMe from '../../public/assets/svgo/undraw_source_code.svg';
+import ImageBlogHeader from '../../public/assets/svgo/undraw_typewriter.svg';
+import ImageServicesHeader from '../../public/assets/svgo/undraw_working.svg';
 
 export {
+  ImageAboutHeader,
+  ImageAboutMe,
+  ImageBlogHeader,
+  ImageContactHeader,
   ImageHomeBlog,
   ImageHomeHeader,
   ImageHomeProgramming,
   ImageHomeProjects,
   ImageHomeServices,
+  ImageProjectsHeader,
+  ImageServicesHeader,
 };
