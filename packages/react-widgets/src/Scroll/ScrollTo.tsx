b04f4817c82b24153af3603ba7e1128c9d45359e
MenSeb <sebastien.menard.github@gmail.com>
2023-08-04 15:51:53 -0400

    chore(widgets): update scroll to components logic

--- packages/react-widgets/src/Scroll/ScrollTo.tsx
+++ packages/react-widgets/src/Scroll/ScrollTo.tsx
@@ -1,22 +1,45 @@
 import * as React from 'react';
+import classNames from 'classnames';
 import { MdArrowLeft, MdArrowUpward } from 'react-icons/md';
-import { useMatchMedia } from '@packages/react-hooks';
+import { useEventListener, useMatchMedia } from '@packages/react-hooks';
 import { Scroll, ScrollProps } from '.';
 
 export const QUERY_REDUCED_MOTION = '(prefers-reduced-motion: reduce)';
 
-export type ScrollToProps = ScrollProps;
+export type ScrollToProps = ScrollProps & {
+  scrollDirection: 'scrollLeft' | 'scrollTop';
+  scrollHidden?: boolean;
+};
 
-export function ScrollTo({ scrollOptions, ...props }: ScrollToProps) {
+export function ScrollTo({
+  className,
+  scrollDirection,
+  scrollHidden,
+  scrollOptions,
+  ...props
+}: ScrollToProps) {
   const { matches } = useMatchMedia(QUERY_REDUCED_MOTION, () =>
     setMotion((motion) => !motion),
   );
 
   const [motionReduced, setMotion] = React.useState(matches);
 
-  return (
+  const getScrolled = React.useCallback(() => {
+    return document.documentElement[scrollDirection] > 0 ? false : true;
+  }, [scrollDirection]);
+
+  const [scrolled, setScrolled] = React.useState(getScrolled);
+
+  const scroll = React.useCallback(() => {
+    setScrolled(getScrolled);
+  }, [getScrolled, setScrolled]);
+
+  useEventListener(window, 'scroll', scroll);
+
+  return scrollHidden && scrolled ? null : (
     <Scroll
       {...props}
+      className={classNames(className, { scrolled })}
       scrollOptions={{
         ...scrollOptions,
         behavior: motionReduced ? 'instant' : 'smooth',
@@ -32,7 +55,9 @@ export function ScrollToTop({
   return (
     <ScrollTo
       {...props}
+      aria-label="scroll to top"
       className="scroll-top"
+      scrollDirection="scrollTop"
       scrollOptions={{ ...scrollOptions, top: 0 }}
     >
       <MdArrowUpward className="scroll-icon" role="presentation" />
@@ -47,7 +72,9 @@ export function ScrollToLeft({
   return (
     <ScrollTo
       {...props}
+      aria-label="scroll to left"
       className="scroll-left"
+      scrollDirection="scrollLeft"
       scrollOptions={{ ...scrollOptions, left: 0 }}
     >
       <MdArrowLeft className="scroll-icon" role="presentation" />
