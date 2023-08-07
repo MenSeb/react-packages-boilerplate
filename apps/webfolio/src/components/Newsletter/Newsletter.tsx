0ce2046c91ecd2d2c3a04f230c769e85e5fbc8bf
MenSeb <sebastien.menard.github@gmail.com>
2023-08-07 09:47:18 -0400

    chore(newsletter): extract & style component Newsletter

---
+++ apps/webfolio/src/components/Newsletter/Newsletter.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import * as UI from '@packages/react-ui';
+
+export function Newsletter() {
+  return (
+    <UI.Container className="newsletter">
+      <UI.Heading className="newsletter-title" level={2}>
+        Newsletter
+      </UI.Heading>
+      <UI.Text className="newsletter-text">
+        Get notified of the new articles in my blog, the exclusive promotions on
+        my services and the latest projects brought to life, all conveniently in
+        your inbox.
+      </UI.Text>
+      <UI.Form
+        onSubmit={(e: React.FormEvent) => e.preventDefault()}
+        className="newsletter-form"
+      >
+        <UI.Input
+          className="newsletter-input"
+          type="email"
+          placeholder="Your email..."
+        />
+        <UI.Input
+          className="newsletter-input"
+          type="text"
+          placeholder="Your name..."
+        />
+        <UI.Input
+          className="newsletter-input"
+          type="submit"
+          value="subscribe"
+        />
+        <UI.Text className="newsletter-info">
+          You may unsubscribe at any time.
+        </UI.Text>
+      </UI.Form>
+    </UI.Container>
+  );
+}
