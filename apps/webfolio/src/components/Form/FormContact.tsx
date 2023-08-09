0aaa27c6a85b57e146b066001631395e9a6f64c7
MenSeb <sebastien.menard.github@gmail.com>
2023-08-08 21:09:03 -0400

    chore(contact): initiate contact form logic

---
+++ apps/webfolio/src/components/Form/FormContact.tsx
@@ -0,0 +1,91 @@
+import * as React from 'react';
+import * as UI from '@packages/react-ui';
+import { log } from 'console';
+
+export type FetchContactData = {
+  data: { status: string };
+  error?: never;
+};
+
+export type FetchContactError = {
+  data?: never;
+  error: { message: string };
+};
+
+export type FetchContactEmail = FetchContactData | FetchContactError;
+
+export type ContactEmailResponse = Omit<Response, 'json'> & {
+  json: () => Promise<FetchContactEmail>;
+};
+
+async function sendContactEmail(formData: FormData) {
+  const response: ContactEmailResponse = await fetch(
+    'http://localhost:3001/form-contact',
+    {
+      body: JSON.stringify(Object.fromEntries(formData)),
+      headers: { 'Content-type': 'application/json' },
+      method: 'POST',
+    },
+  );
+
+  return response.json();
+}
+
+export function FormContact() {
+  const onSubmit = React.useCallback<React.FormEventHandler<HTMLFormElement>>(
+    async (event) => {
+      event.preventDefault();
+
+      const formData = new FormData(event.currentTarget);
+
+      const { data, error } = await sendContactEmail(formData);
+
+      log({ data, error });
+    },
+    [],
+  );
+
+  return (
+    <UI.Form className="contact-form" onSubmit={onSubmit}>
+      <UI.Label>
+        First Name
+        <UI.Input
+          name="firstname"
+          type="text"
+          placeholder="Your first name..."
+        />
+      </UI.Label>
+      <UI.Label>
+        Last Name
+        <UI.Input name="lastname" type="text" placeholder="Your last name..." />
+      </UI.Label>
+      <UI.Label>
+        Email
+        <UI.Input name="email" type="email" placeholder="Your email..." />
+      </UI.Label>
+      <UI.Label>
+        Website
+        <UI.Select name="website">
+          <option value="">Your website...</option>
+          <option>Blog</option>
+          <option>Business</option>
+          <option>E-Commerce</option>
+          <option>Portfolio</option>
+          <option>Brochure</option>
+          <option>Catalogue</option>
+          <option>Educational</option>
+          <option>Informational</option>
+          <option>Infopreneur</option>
+          <option>Non-profit</option>
+          <option>Personal</option>
+          <option>Other</option>
+        </UI.Select>
+      </UI.Label>
+      <UI.Label>
+        Project
+        <UI.Textarea name="project" placeholder="Your project..." />
+      </UI.Label>
+      <UI.Input type="submit" value="Send message" />
+    </UI.Form>
+  );
+}
