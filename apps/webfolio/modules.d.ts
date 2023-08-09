20827e6498551df2e99c0975c7cfb433638f678b
MenSeb <sebastien.menard.github@gmail.com>
2023-08-08 20:03:13 -0400

    chore(env): setup types for process.env

--- apps/webfolio/modules.d.ts
+++ apps/webfolio/modules.d.ts
@@ -3,3 +3,15 @@ declare module '*.svg' {
   export const Icon: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default Icon;
 }
+
+declare module NodeJS {
+  export interface ProcessEnv {
+    [key: string]: string | undefined;
+    EXPRESS_PORT: string;
+    NODE_MAILER_EMAIL: string;
+    NODE_MAILER_HOST: string;
+    NODE_MAILER_PORT: string;
+    NODE_MAILER_USERNAME: string;
+    NODE_MAILER_PASSWORD: string;
+  }
+}
