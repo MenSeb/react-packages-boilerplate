08b4afddfbb829c004bf302d9fee3759026f444a
MenSeb <sebastien.menard.github@gmail.com>
2023-08-10 22:54:53 -0400

    test(form): rename Error to Alert

--- packages/react-form/__tests__/components/Alert.test.tsx
+++ packages/react-form/__tests__/components/Alert.test.tsx
@@ -6,17 +6,17 @@ const props = {
   children: 'children',
 };
 
-const renderError = createRender<AlertProps>(Alert, { props });
+const renderAlert = createRender<AlertProps>(Alert, { props });
 
-function getError() {
+function getAlert() {
   return screen.getByRole('alert');
 }
 
 describe('<Alert />', () => {
   it('renders correctly', () => {
-    renderError();
+    renderAlert();
 
-    expect(getError()).toBeInTheDocument();
-    expect(getError()).toHaveTextContent(props.children);
+    expect(getAlert()).toBeInTheDocument();
+    expect(getAlert()).toHaveTextContent(props.children);
   });
 });
