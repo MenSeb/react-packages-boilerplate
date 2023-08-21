11176a879bf3e31ae24897cb179a04188dd0545a
MenSeb <sebastien.menard.github@gmail.com>
2023-08-21 15:47:21 -0400

    chore(wrapper): update & test wrapper helpers

--- packages/react-test/__tests__/wrapper.test.tsx
+++ packages/react-test/__tests__/wrapper.test.tsx
@@ -1,61 +1,16 @@
-import * as React from 'react';
-import { screen } from '@testing-library/react';
-import { createRender, createRenderWrapper, createWrapper } from '../src';
-
-interface Props extends React.PropsWithChildren {
-  className: string;
-}
-
-function Provider(props: Props) {
-  return <div {...props} data-testid="wrapper" />;
-}
-
-function Test(props: Props) {
-  return <div {...props} data-testid="test" />;
-}
-
-function getTest() {
-  return screen.getByTestId('test');
-}
-
-function getWrapper() {
-  return screen.getByTestId('wrapper');
-}
-
-const propsProvider = {
-  className: 'className',
-};
-
-const propsWrapper = {
-  children: <Test className="test" />,
-};
+import { createRender, createWrapper } from '../src';
+import { getWrapper, propsWrapper, Wrapper } from '.';
 
 describe('createWrapper', () => {
-  const render = createRender(
-    createWrapper(Provider, propsProvider),
-    propsWrapper,
-  );
+  const render = createRender(createWrapper(Wrapper, propsWrapper), {
+    children: 'children',
+  });
 
   it('renders with props and wrapper', () => {
     render();
 
     expect(getWrapper()).toBeInTheDocument();
-    expect(getWrapper()).toContainElement(getTest());
-    expect(getWrapper()).toHaveClass(propsProvider.className);
-  });
-});
-
-describe('createRenderWrapper', () => {
-  const createRender = createRenderWrapper(Provider, propsProvider);
-
-  const renderWrapper = createRender(Test, propsProvider);
-
-  it('renders with props and wrapper', () => {
-    renderWrapper();
-
-    expect(getWrapper()).toBeInTheDocument();
-    expect(getWrapper()).toContainElement(getTest());
-    expect(getWrapper()).toHaveClass(propsProvider.className);
-    expect(getTest()).toHaveClass(propsProvider.className);
+    expect(getWrapper()).toHaveTextContent('children');
+    expect(getWrapper()).toHaveClass(propsWrapper.className);
   });
 });
